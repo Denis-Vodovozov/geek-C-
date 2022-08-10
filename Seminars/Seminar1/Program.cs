9c9bbd1e6c5b770005d3cea4545290520a0b8bd6
Denis Vodovozov <denniboy@yandex.ru>
2022-08-10 23:19:59 +0300

    добавлена задача 1

---
+++ Seminars/Seminar1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+// Задача 1.
+
+Console.Write("введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int quad = num * num;
+
+Console.WriteLine($"квадрат из числа {num} это {quad}");
+*/
+
+
+// Задача 2.
+
+
