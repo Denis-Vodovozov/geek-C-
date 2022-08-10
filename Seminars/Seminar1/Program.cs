86cec43cbe4e3c4d8b27830cb39cc6ed16cf914c
Denis Vodovozov <denniboy@yandex.ru>
2022-08-10 23:37:09 +0300

    добавлена задача 2

--- Seminars/Seminar1/Program.cs
+++ Seminars/Seminar1/Program.cs
@@ -12,4 +12,18 @@ Console.WriteLine($"квадрат из числа {num} это {quad}");
 
 // Задача 2.
 
+Console.Write("введите первое число ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите второе число ");
+int n2 = Convert.ToInt32(Console.ReadLine());
 
+int quad2 = n2 * n2;
+
+if (n1 == quad2)
+{
+    Console.WriteLine($"число {n1} это квадрат числа {n2}");
+}
+else
+{
+    Console.WriteLine($"число {n1} это не квадрат числа {n2}");
+}
