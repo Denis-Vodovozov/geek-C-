b5321f12c61c7b6bc5054560cbc2143576bbdf4c
Denis Vodovozov <denniboy@yandex.ru>
2022-09-18 20:02:44 +0300

    добавлены решения задач для дз8

--- HomeWorks/Work8/Program.cs
+++ HomeWorks/Work8/Program.cs
@@ -14,14 +14,13 @@ void Show2dArray(int[,] array)
     {
         for(int j = 0; j < array.GetLength(1); j++)
             Console.Write(array[i,j] + " ");
-        
         Console.WriteLine();
     }
 }
-int[,] Sort2dArray(int[,] array)
+int[,] SortArray(int[,] array)
 {
     for(int i = 0; i < array.GetLength(0); i++)
-        for(int k = 0; k < array.GetLength(1); k++)
+        for(int z = 0; z < array.GetLength(1) - 1; z++)
             for(int j = 0; j < array.GetLength(1) - 1; j++)
                 {
                     if (array[i, j] > array[i, j + 1])
@@ -44,54 +43,47 @@ int max = Convert.ToInt32(Console.ReadLine());
 int[,] myArray = CreateRandom2dArray(m, n, min, max);
 Show2dArray(myArray);
 Console.WriteLine();
-Show2dArray(Sort2dArray(myArray));
+Show2dArray(SortArray(myArray));
 */
-
 
 /*
 // Задача 56. Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
 {
     int[,] newArray = new int[rows, columns];
-    for(int i = 0; i < rows; i++)
-        for(int j = 0; j < columns; j++)
-            newArray[i,j] = new Random().Next(minValue, maxValue + 1);
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            newArray[i, j] = new Random().Next(minValue, maxValue + 1);
     return newArray;
 }
 void Show2dArray(int[,] array)
 {
-    for(int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for(int j = 0; j < array.GetLength(1); j++)
-            Console.Write(array[i,j] + " ");
-        
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
         Console.WriteLine();
     }
 }
-int FindMinSumRow(int[,] array)
+int FindMinSumElementsRow(int[,] array)
 {
     int sum = 0;
     int resultSum = 0;
-    int rowNumber = -1;
-    for(int j = 0; j < array.GetLength(1); j++)
+    int rowNumber = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+        resultSum += array[0, j];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            sum += array[i, j];
+        if (sum < resultSum)
         {
-            resultSum = resultSum + array[0, j];
-            rowNumber = 0;
+            resultSum = sum;
+            rowNumber = i;
         }
-    for(int i = 1; i < array.GetLength(0); i++)
-            {
-                for(int j = 0; j < array.GetLength(1); j++)
-                    {
-                        sum = sum + array[i, j];
-                    }
-                if(sum < resultSum) 
-                    {
-                        resultSum = sum;
-                        rowNumber = i;
-                    }
-            }
-        
-        return rowNumber;
+        sum = 0;
+    }
+    return rowNumber + 1;
 }
 Console.Write("Input number of rows: ");
 int m = Convert.ToInt32(Console.ReadLine());
@@ -104,39 +96,38 @@ int max = Convert.ToInt32(Console.ReadLine());
 int[,] myArray = CreateRandom2dArray(m, n, min, max);
 Show2dArray(myArray);
 Console.WriteLine();
-int minSumOfRowNumber = (FindMinSumRow(myArray));
-Console.WriteLine($"The index of row with minimum sum of elements is {minSumOfRowNumber}");
+int minSumRowNumber = (FindMinSumElementsRow(myArray));
+Console.WriteLine($"The number of row with minimum sum of elements is {minSumRowNumber}");
 */
-
 
 /*
 // Задача 58. Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
 {
     int[,] newArray = new int[rows, columns];
-    for(int i = 0; i < rows; i++)
-        for(int j = 0; j < columns; j++)
-            newArray[i,j] = new Random().Next(minValue, maxValue + 1);
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            newArray[i, j] = new Random().Next(minValue, maxValue + 1);
     return newArray;
 }
 void Show2dArray(int[,] array)
 {
-    for(int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for(int j = 0; j < array.GetLength(1); j++)
-            Console.Write(array[i,j] + " ");
-        
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
         Console.WriteLine();
     }
 }
-int[,] Multiplication(int[,] a, int[,] b)
+int[,] Multiply2dArrays(int[,] a, int[,] b)
 {
     int[,] m = new int[a.GetLength(0), b.GetLength(1)];
-    if (a.GetLength(1) != b.GetLength(0)) 
-        {
-            Console.WriteLine("These matrices cannot be multiplied");
-            return m;
-        }
+    if (a.GetLength(1) != b.GetLength(0))
+    {
+        Console.WriteLine("These arrays cannot be multiplied");
+        return m;
+    }
     else
     {
         for (int i = 0; i < a.GetLength(0); i++)
@@ -144,14 +135,11 @@ int[,] Multiplication(int[,] a, int[,] b)
             for (int j = 0; j < b.GetLength(1); j++)
             {
                 for (int k = 0; k < b.GetLength(0); k++)
-                {    
-                    m[i, j] += a[i, k] * b[k, j];                
-                }
+                    m[i, j] += a[i, k] * b[k, j];
             }
         }
         return m;
     }
-    
 }
 Console.Write("Input number of rows of first array: ");
 int m1 = Convert.ToInt32(Console.ReadLine());
@@ -171,50 +159,49 @@ Show2dArray(firstArray);
 Console.WriteLine();
 Show2dArray(secondArray);
 Console.WriteLine();
-int[,] multipliedArray = Multiplication(firstArray, secondArray);
-Show2dArray(multipliedArray);
+int[,] multiplyArrays = Multiply2dArrays(firstArray, secondArray);
+Show2dArray(multiplyArrays);
 */
-
 
 /*
 // Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, 
 // которая будет построчно выводить массив, добавляя индексы каждого элемента.
 int[,,] MakeNumbersUnique(int x, int y, int z, int minValue, int maxValue)
-{                 
-    if(x * y * z >= maxValue + 1 - minValue) 
-        {
-            Console.WriteLine("An array of a given size cannot be filled with unique elements. Change array size or value range.");
-            int[,,] array = new int[0, 0, 0];
-            return array;
-        }
+{
+    if (x * y * z >= maxValue + 1 - minValue)
+    {
+        Console.WriteLine("An array of a given size cannot be filled with unique elements. Change array size or value range.");
+        int[,,] array = new int[0, 0, 0];
+        return array;
+    }
     else
-        {
-            int[,,] array = new int[x, y, z];
-            for(int i = 0; i < x; i++)
-                for(int j = 0; j < y; j++)
-                    for(int k = 0; k < z; k++)
-                        {
-                            int currentNumber = array[i, j, k];
-                            
-                            for(int count = 0; count < x * y * z; count++)
+    {
+        int[,,] array = new int[x, y, z];
+        for (int i = 0; i < x; i++)
+            for (int j = 0; j < y; j++)
+                for (int k = 0; k < z; k++)
+                {
+                    int currentNumber = array[i, j, k];
+
+                    for (int count = 0; count < x * y * z; count++)
+                    {
+                        for (int a = 0; a < x; a++)
+                            for (int b = 0; b < y; b++)
+                                for (int c = 0; c < z; c++)
                                 {
-                                    for(int a = 0; a < x; a++)
-                                        for(int b = 0; b < y; b++)
-                                            for(int c = 0; c < z; c++)
-                                                {
-                                                    if(currentNumber == array[a, b, c])
-                                                        {
-                                                            currentNumber = new Random().Next(minValue, maxValue + 1);
-                                                            a = 0;
-                                                            b = 0;
-                                                            c = 0;
-                                                            count = 0;
-                                                        }
-                                                }
-                                }                
-                            array[i, j, k] = currentNumber;
-                        }
-            return array;
+                                    if (currentNumber == array[a, b, c])
+                                    {
+                                        currentNumber = new Random().Next(minValue, maxValue + 1);
+                                        a = 0;
+                                        b = 0;
+                                        c = 0;
+                                        count = 0;
+                                    }
+                                }
+                    }
+                    array[i, j, k] = currentNumber;
+                }
+        return array;
     }
 }
 void Show3dArray(int[,,] array)
@@ -228,81 +215,81 @@ void Show3dArray(int[,,] array)
                 Console.WriteLine($"{array[i, j, k]} ({i}, {j}, {k})");
             }
         }
-    }   
+    }
 }
-Console.Write("Input first dimension size: ");
+
+Console.Write("Input size of first dimension: ");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input second dimension size: ");
+Console.Write("Input size of second dimension: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input third dimension size: ");
-int p = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input size of third dimension: ");
+int b = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input min possible value: ");
 int min = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input max possible value: ");
 int max = Convert.ToInt32(Console.ReadLine());
-int[,,] uniqueArray = MakeNumbersUnique(m, n, p, min, max);
+int[,,] uniqueArray = MakeNumbersUnique(m, n, b, min, max);
 Show3dArray(uniqueArray);
 */
 
 /*
 // Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
-int[,] CreateSpiralArray(int rows, int columns)
+int[,] CreateSpiral2dArray(int rows, int columns)
 {
     int[,] spiralArray = new int[rows, columns];
     int number = 1;
-    for(int k = 0; k < rows / 2; k++)
+    for (int k = 0; k < rows / 2; k++)
+    {
+        int i = k;
+        int j = k;
+        for (j = k; j < columns - k; j++)
         {
-            int i = k;
-            int j = k;
-            for(j = k; j < columns - k; j++)
-                {
-                    if(number <= rows * columns)
-                        {
-                            spiralArray[i, j] = number;
-                            number++;
-                        }
-                    else return spiralArray;
-                }
-            j = columns - k - 1;
-            for(i = k + 1; i < rows - k; i++)
-                {
-                    if(number <= rows * columns)
-                        {
-                            spiralArray[i, j] = number;
-                            number++;
-                        }
-                    else return spiralArray;
-                }
-            i = rows - k - 1;
-            for (j = columns - k - 2; j >= k; j--)
-                {
-                    if(number <= rows * columns)
-                        {
-                            spiralArray[i, j] = number;
-                            number++;
-                        }
-                    else return spiralArray;
-                }  
-            j = k;
-            for(i = rows - k - 2; i > k; i--)
-                {
-                    if(number <= rows * columns)
-                        {
-                            spiralArray[i, j] = number;
-                            number++;
-                        }
-                    else return spiralArray;
-                }          
+            if (number <= rows * columns)
+            {
+                spiralArray[i, j] = number;
+                number++;
+            }
+            else return spiralArray;
         }
+        j = columns - k - 1;
+        for (i = k + 1; i < rows - k; i++)
+        {
+            if (number <= rows * columns)
+            {
+                spiralArray[i, j] = number;
+                number++;
+            }
+            else return spiralArray;
+        }
+        i = rows - k - 1;
+        for (j = columns - k - 2; j >= k; j--)
+        {
+            if (number <= rows * columns)
+            {
+                spiralArray[i, j] = number;
+                number++;
+            }
+            else return spiralArray;
+        }
+        j = k;
+        for (i = rows - k - 2; i > k; i--)
+        {
+            if (number <= rows * columns)
+            {
+                spiralArray[i, j] = number;
+                number++;
+            }
+            else return spiralArray;
+        }
+    }
     return spiralArray;
 }
 void Show2dArray(int[,] array)
 {
-    for(int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for(int j = 0; j < array.GetLength(1); j++)
-            Console.Write(array[i,j] + " ");
-        
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
         Console.WriteLine();
     }
 }
@@ -310,6 +297,6 @@ Console.Write("Input number of rows: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input number of columns: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int[,] mySpiralArray = CreateSpiralArray(m, n);
-Show2dArray(mySpiralArray);
+int[,] SpiralArray = CreateSpiral2dArray(m, n);
+Show2dArray(SpiralArray);
 */
